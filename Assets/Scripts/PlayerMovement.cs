13e573d588c01bac3205fdf144bb5319f06b5307
Finnley111 <86789658+Finnley111@users.noreply.github.com>
2024-01-20 20:57:29 -0500

    fixed bug where powers would transfer after death and during level change

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -129,6 +129,14 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log("Player is dead");
             killPlayer();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            GameManager.pegsActive = new Dictionary<string, bool>(){
+            {"up", false},
+            {"down", false},
+            {"right", false},
+            {"left", false},
+            {"dash", false},
+            {"start", false},
+            };
             // }
         }
 
@@ -186,6 +194,14 @@ public class PlayerMovement : MonoBehaviour
         if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
         {
             SceneManager.LoadScene(nextSceneIndex);
+            GameManager.pegsActive = new Dictionary<string, bool>(){
+            {"up", false},
+            {"down", false},
+            {"right", false},
+            {"left", false},
+            {"dash", false},
+            {"start", false},
+            };
         }
         else
         {
